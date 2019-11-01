47c357a892ef312766288eacf319d6baf9ef10dc
Elin Alm <elin95alm@gmail.com>
2019-11-01 10:48:18 +0100

    början på spelet

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+function clickMe(){
+    let welcome = prompt('Nu börjar vi spela! är du redo?')
+
+
+    if(welcome === 'yes'){
+        prompt('Härligt då kör vi igång!')
+    }
+    else {
+        prompt('Tråkigt, vi ses en annan gång!')
+    }
+
+}
